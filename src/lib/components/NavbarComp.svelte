c4a8a37ac252ed1dcaf3e8f086646567fe6e44bc
unknown <mikatzukiathanas@gmail.com>
2026-02-20 16:58:46 +0900

    reconfigure cicd for static page mode

--- src/lib/components/NavbarComp.svelte
+++ src/lib/components/NavbarComp.svelte
@@ -9,7 +9,7 @@
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">3MWorkshop</span>
         </a>
     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <ul class="font-medium flex flex-col px-4 py-2 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li>
             <a class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" href="#About">
             About
