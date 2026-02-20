0ac0bd5f83a73168a083d08211cfad85de34e290
unknown <mikatzukiathanas@gmail.com>
2026-02-20 16:44:34 +0900

    change to static page

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,16 @@
-import adapter from "svelte-adapter-bun"; 
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-
-  kit: {
-    adapter: adapter(),
-  },
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	}
 };
 
 export default config;
