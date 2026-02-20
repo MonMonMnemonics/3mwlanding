87ccd3c67d5f1ba263869a5f6949692f49244558
MonMonMnemonics <72958130+MonMonMnemonics@users.noreply.github.com>
2026-02-20 17:03:12 +0900

    Update FooterComp.svelte

--- src/lib/components/FooterComp.svelte
+++ src/lib/components/FooterComp.svelte
@@ -5,8 +5,12 @@
 
 <div class="w-screen border-gray-200 bg-gray-900 font-inter">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <div class="font-roboto flex items-center">
-        Contact me at <a href="mailto:mikatzukiathanas@gmail.com" class="ml-1"><Fa icon={faEnvelope} style="color: white"/> mikatzukiathanas@gmail.com</a>
+    <div class="font-roboto flex items-center flex flex-row items-center gap-1">
+        <div>Contact me at </div>
+        <a href="mailto:mikatzukiathanas@gmail.com" class="ml-1 flex flex-row items-center gap-1">
+            <Fa icon={faEnvelope} style="color: white"/>
+            <div> mikatzukiathanas@gmail.com</div>
+        </a>
     </div>
     </div>
 </div> 
